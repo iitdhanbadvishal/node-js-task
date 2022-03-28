50b40ffcd86799c837c51ce39bea731a4cc1bf00
iitdhanbavishal <vishal.iitdh@gmail.com>
2022-03-28 20:32:21 +0530

    added access token for get requests

--- movies-server/src/app.js
+++ movies-server/src/app.js
@@ -70,7 +70,7 @@ app.get("/movies", async (req, res) => {
 
     return res.status(200).json(createdMovies);
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(401).send(error.message);
   }
 });
 
